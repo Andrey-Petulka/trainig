1e98452167a236fc1a1cab2efb0868f68c929b7c
Andrey <andreonof@yandex.ru>
2024-06-16 21:25:00 +0300

    Тренируюсь

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
-let logic = document.querySelectorAll('p');
-console.log(logic)
-let letters = ['А', 'Б', 'В']
+const answer = prompt('Какую машину вы предпочитаете?');
+let index = 0;
+let age = 5;
 
-console.log('letters before:' , letters)
+function hello(answer, age){
+    index++
+    console.log(index, answer, age++);
+}
 
-letters.unshift('Г', 'ГЫГЫГЫГЫ', true, logic)
-console.log('letters after:', letters)
-console.log(letters.shift())
-console.log('letters before:' , letters)
+for(let index = 1; index < 11; index ++ || age++){
+    hello(answer, 18)
+    hello('Привет', 99)
+}
