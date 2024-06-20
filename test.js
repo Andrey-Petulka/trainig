991fda887473c32986b8c0c8294740f296ff0083
Andrey <andreonof@yandex.ru>
2024-06-20 21:37:47 +0300

    Фиксация

--- test.js
+++ test.js
@@ -1,13 +1,7 @@
-const answer = prompt('Какую машину вы предпочитаете?');
-let index = 0;
-let age = 5;
+function hello (param){
+    console.log('Тело функции');
+    console.log(param)
+};
 
-function hello(answer, age){
-    index++
-    console.log(index, answer, age++);
-}
-
-for(let index = 1; index < 11; index ++ || age++){
-    hello(answer, 18)
-    hello('Привет', 99)
-}
+// hello.call(null, 'Переопределенный параментр функции');
+setTimeout(() => hello('Результат вызова функции'), 0);
